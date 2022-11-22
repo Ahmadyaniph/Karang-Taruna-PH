23c5bc3588dfe4a465ecef419e0e0d8f234a0620
Ahmad Yani <117455523+Ahmadyaniph@users.noreply.github.com>
2022-11-22 06:09:11 +0000

    menambahkan settings.json dan melanjutkan menulis sorch code pada file scrips.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const navbar = document.getElementsByTagName('nav')[0];
 window.addEventListener('scroll', function() {
     console.log(window.scrollY);
+    if (window.scrollY>1){
+        navbar.classList.replace('fixed-top','position-static');
+    }
 });
