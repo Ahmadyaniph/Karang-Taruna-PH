e5c255b58cfb178afcd3a3cb972b8d547a92cd0d
Ahmad Yani <117455523+Ahmadyaniph@users.noreply.github.com>
2022-11-24 14:15:39 +0000

    mengecek file script.js dan index.html

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const navbar = document.getElementsByTagName('nav')[0];
 window.addEventListener('scroll', function() {
-    
+    console.log(window.scrollY);
     if (window.scrollY>1){
-        navbar.classList.replace('fixed-top','position-static');
+        navbar.classList.replace('position-fixed','position-static');
     } else if (window.screenX>1){
-        navbar.classList.replace('position-static','fixed-top');
+        navbar.classList.replace('position-static','position-fixed');
     }
 });
