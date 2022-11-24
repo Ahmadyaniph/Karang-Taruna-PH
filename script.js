e06a2551cc4e2809f9e8dfe021a1fa020970ce0d
Ahmad Yani <117455523+Ahmadyaniph@users.noreply.github.com>
2022-11-24 13:51:19 +0000

    mengecek file script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const navbar = document.getElementsByTagName('nav')[0];
 window.addEventListener('scroll', function() {
-    // console.log(window.scrollY);
+    
     if (window.scrollY>1){
         navbar.classList.replace('fixed-top','position-static');
     } else if (window.screenX>1){
