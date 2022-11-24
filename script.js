3954b293413236ff32629b942525824e39de0609
Ahmad Yani <117455523+Ahmadyaniph@users.noreply.github.com>
2022-11-24 14:31:27 +0000

    menambahkan responsive colom pada tentang di file index.html

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const navbar = document.getElementsByTagName('nav')[0];
 window.addEventListener('scroll', function() {
     console.log(window.scrollY);
     if (window.scrollY>1){
-        navbar.classList.replace('position-fixed','position-static');
+        navbar.classList.replace('fixed-top','position-static');
     } else if (window.screenX>1){
-        navbar.classList.replace('position-static','position-fixed');
+        navbar.classList.replace('position-static','fixed-top');
     }
 });
